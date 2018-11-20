de4c0c2caec5fd4b3a31ebb8c3e8cc65fd6dda7d
Marta Hendel <marta.hendel@gmail.com>
2018-11-20 22:17:02 +0000

    Adding calculator app to my github pages

---
+++ calculator/precache-manifest.bb00e6d1110e259d18b0605b6bb031c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "acfa24e6db7f0afdbdf4",
+    "url": "/static/js/main.acfa24e6.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "acfa24e6db7f0afdbdf4",
+    "url": "/static/css/main.79f7c264.chunk.css"
+  },
+  {
+    "revision": "6e134cb10493b853de297c5347a39537",
+    "url": "/index.html"
+  }
+];
